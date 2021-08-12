84bf53440cf186041a6947102b3ff79084b6016c
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 22:15:59 +0600

    Create footer.blade.php
    footer blade added

---
+++ resources/views/livewire/master/footer.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div class="container">
+        <div class="row">
+            <footer class="footer mt-auto py-3">
+                <div class="container">
+                    <div style="background-color: white">
+                        <span style="display: flex; justify-content:center;">
+                            <h4>
+                                Copyright @ Arik Md Isthiaque
+                            </h4>
+                        </span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</div>
