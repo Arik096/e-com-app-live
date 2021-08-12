1f5df30d8d8d1f84e56ea0af557253dd0fa9a44b
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 22:12:28 +0600

    Create Home.php
    home class added

---
+++ app/Http/Livewire/Master/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Master;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.master.home');
+    }
+}
