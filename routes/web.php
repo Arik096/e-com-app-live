8e7bae27ce79ce46576b505f3f83065abbb60a25
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 22:13:28 +0600

    Update web.php
    home page route added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Livewire\Master\Home;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class)->name('HomePage');
