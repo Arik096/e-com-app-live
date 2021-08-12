34f24ce2be2549d81987134df5c15f56e5627129
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 22:15:48 +0600

    Create Footer.php
    footer livewire class added

---
+++ app/Http/Livewire/Master/Footer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Master;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return view('livewire.master.footer');
+    }
+}
