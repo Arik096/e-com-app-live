df0d36ca70b69c0fcfebb643abd4b0c9c78093b0
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-12 22:12:50 +0600

    Create header.blade.php
    header blade

---
+++ resources/views/livewire/master/header.blade.php
@@ -0,0 +1,79 @@
+<div>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="{{ asset('img/arikp_logo-removebg-preview.png') }}" alt="logo"
+                            style="max-height: 50px;">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Orders</a>
+                            </li>
+                            <li class="nav-item" style="display: flex;align-items: center;">
+                                <a class="nav-link active" aria-current="page" href="#">cart</a>
+                                <input type="text" id="cart" name="cart" style="max-width: 50px;max-height:30px;"
+                                    value="10" disabled>
+                            </li>
+                        </ul>
+                        <form class="d-flex me-2 mb-2 mb-lg-0">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-primary" type="submit">Search</button>
+                        </form>
+                        @if (Session::has('user'))
+                            @php
+                                $user[] = Session::get('user');
+                            @endphp
+                            <ul class="navbar-nav me-2 mb-2 mb-lg-0">
+                                <li class="nav-item me-2">
+                                    <button class="btn btn-outline-primary">
+                                        {{ $user[0]->name }}
+                                    </button>
+                                </li>
+                            </ul>
+                        @endif
+                        @if (!Session::has('user'))
+                            <ul class="navbar-nav me-2 mb-2 mb-lg-0">
+                                <li class="nav-item me-2">
+                                    <a href="#">
+                                        <button class="btn btn-outline-primary">
+                                            Login
+                                        </button>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <button class="btn btn-outline-primary">Register</button>
+                                </li>
+                            </ul>
+                        @endif
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <style>
+        ul .nav-item {
+            font-size: 25px;
+            font-weight: 800;
+        }
+
+        ul .nav-item a {
+            text-decoration: none !important;
+        }
+
+        ul .nav-item a:hover {
+            color: white;
+        }
+
+    </style>
+</div>
