4ac349ade896bfdbf15b23f909b6ea44145d93c6
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-15 13:45:02 +0600

    Create Register.php
    register livewire class created

---
+++ app/Http/Livewire/User/Register.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Register extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.register');
+    }
+}
