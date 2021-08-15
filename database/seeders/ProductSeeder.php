dba8841ce68aed475576225ee049c96ed4331f79
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-15 13:45:17 +0600

    Update ProductSeeder.php
    updated productSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -21,7 +22,7 @@ class ProductSeeder extends Seeder
         'description' => 'Soap is a salt of a fatty acid used in a variety of cleansing and lubricating products. In a
         domestic setting, soaps are surfactants usually used for washing, bathing, and other types of housekeeping. In
         industrial settings, soaps are used as thickeners, components of some lubricants, and precursors to catalysts.',
-        'img_path' => '\img\arikp_logo-removebg-preview.png'
+        'img_path' => '/img/arikp_logo-removebg-preview.png'
         ]);
     }
 }
