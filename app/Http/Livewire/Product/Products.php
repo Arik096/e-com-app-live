c74b0203699d3e183d16c32aea175d831f95e71d
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-08-16 21:13:18 +0600

    Update Products.php

--- app/Http/Livewire/Product/Products.php
+++ app/Http/Livewire/Product/Products.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Http\Livewire\Product;
-use Illuminate\Support\Facades\DB;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+
 
 class Products extends Component
 {
     public function render()
     {
         $products = DB::table('products')
-        ->get();
-        return view('livewire.product.products',['products'=>$products]);
+            ->get();
+        return view('livewire.product.products', ['products' => $products]);
     }
 }
